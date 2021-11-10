319a9ff1838c3d10cb0330b411ffd5d890af5395
freevuehub <hackish113@gmail.com>
2021-11-10 20:32:58 +0900

    [improved]: table 추가

--- src/component/Table/index.tsx
+++ src/component/Table/index.tsx
@@ -1,36 +1,149 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { css, Theme } from '@emotion/react'
 
-export interface IProps {}
+export interface IProps {
+  headers: (string | { name: string, key: string | number })[]
+  data: any[]
+  className?: string
+  color?: string
+}
 
 const WrapCss = (props: IProps) => (theme: Theme) => css`
-  table {
-    thead {
-      tr {
-        border-radius: 10px;
-        overflow: hidden;
-        th {
-          min-height: 65px;
-          background-color: ${theme.common.primary};
-          color: ${theme.common.white};
-          font-size: 18px;
+  .header {
+    max-width: max-content;
+    border-radius: 10px;
+    overflow: auto;
+    table {
+      background-color: ${props.color || theme.common.primary};
+      width: max-content;
+      thead {
+        tr {
+          th {
+            height: 65px;
+            color: ${theme.common.white};
+          }
+        }
+      }
+    }
+  }
+  .body {
+    max-width: max-content;
+    box-shadow: ${theme.style.shadow};
+    border-radius: 10px;
+    overflow: auto;
+    table {
+      background-color: ${theme.common.white};
+      width: max-content;
+      thead {
+        tr {
         }
       }
     }
   }
 `
 const Table: React.FC<IProps> = (props) => {
+  const header = useRef<HTMLDivElement>(null)
+  const body = useRef<HTMLDivElement>(null)
+
+  const onTableScroll = (event: React.WheelEvent<HTMLDivElement>) => {
+    if (event.currentTarget === header.current) {
+      body.current?.scrollTo(event.currentTarget.scrollLeft, event.currentTarget.scrollTop)
+    } else if (event.currentTarget === body.current) {
+      header.current?.scrollTo(event.currentTarget.scrollLeft, event.currentTarget.scrollTop)
+    }
+  }
+
+  useEffect(() => {
+    if (header.current && body.current) {
+      const thead = header.current.querySelector('thead') as HTMLTableSectionElement
+      const tbody = body.current.querySelector('tbody') as HTMLTableSectionElement
+      const theadRow = thead.children[0] as HTMLTableRowElement
+      const tbodyRow = tbody.children[0] as HTMLTableRowElement
+
+      [...theadRow.cells].forEach((cell, index) => {
+        const thWidth = Math.round(cell.clientWidth) + 1
+        const tdWidth = Math.round(tbodyRow.cells[index].clientWidth) + 1
+
+        if (thWidth > tdWidth) {
+          cell.style.width = `${thWidth}px`
+          tbodyRow.cells[index].style.width = `${thWidth}px`
+        } else {
+          tbodyRow.cells[index].style.width = `${tdWidth}px`
+          cell.style.width = `${tdWidth}px`
+        }
+      })
+    }
+  }, [header, body])
+
   return (
     <div css={WrapCss(props)}>
-      <table>
-        <thead>
-        <tr>
-          <th className="pt-4 pb-5">foo1</th>
-          <th className="pt-4 pb-5">foo2</th>
-          <th className="pt-4 pb-5">foo3</th>
-        </tr>
-        </thead>
-      </table>
+      <div className="header" ref={header} onScroll={onTableScroll}>
+        <table>
+          <thead>
+          <tr>
+            {
+              props.headers
+                .map(
+                  (header) => typeof header === 'string'
+                    ? (
+                      <th key={header}>
+                        <div className="pl-5 pr-5">
+                          {header}
+                        </div>
+                      </th>
+                    )
+                    : (
+                      <th key={header.key}>
+                        <div className="pl-5 pr-5">
+                          {header.name}
+                        </div>
+                      </th>
+                    )
+                )
+            }
+          </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="body mt-3" ref={body} onScroll={onTableScroll}>
+        <table>
+          <tbody>
+          {
+            React.Children.toArray(
+              props.data
+                .map(
+                  (item) => (
+                    <tr>
+                      {
+                        React.Children.toArray(
+                          props.headers
+                            .map(
+                              (header) => typeof header === 'string'
+                                ? (
+                                  <td>
+                                    <div className="pl-5 pr-5 pb-3 pt-3 text--center">
+                                      {item[header]}
+                                    </div>
+                                  </td>
+                                )
+                                : (
+                                  <td>
+                                    <div className="pl-5 pr-5 pb-3 pt-3 text--center">
+                                      {item[header.key]}
+                                    </div>
+                                  </td>
+                                )
+                            )
+                        )
+                      }
+                    </tr>
+                  )
+                )
+            )
+          }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
--- src/style/common.ts
+++ src/style/common.ts
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react'
 
-const gaps = [0, 1, 2, 3, 4, 5]
+const gaps = [0, 1, 2, 3, 4, 5, 'auto']
 const flexOption = ['center', 'start', 'end', 'space-between', 'space-around']
 
 export default css`
@@ -18,19 +18,34 @@ export default css`
     .jc-${item} { justify-content: ${item}; }
   `)}
 
-  ${gaps.map((gap) => `
-    .mt-${gap} { margin-top: ${gap * 5}px; }
-    .mr-${gap} { margin-right: ${gap * 5}px; }
-    .mb-${gap} { margin-bottom: ${gap * 5}px; }
-    .ml-${gap} { margin-left: ${gap * 5}px; }
-    .ma-${gap} { margin: ${gap * 5}px; }
-    
-    .pt-${gap} { padding-top: ${gap * 5}px; }
-    .pr-${gap} { padding-right: ${gap * 5}px; }
-    .pb-${gap} { padding-bottom: ${gap * 5}px; }
-    .pl-${gap} { padding-left: ${gap * 5}px; }
-    .pa-${gap} { padding: ${gap * 5}px; }
-  `)}
+  ${gaps.map((gap) => typeof gap === 'number'
+    ? `
+      .mt-${gap} { margin-top: ${gap * 5}px; }
+      .mr-${gap} { margin-right: ${gap * 5}px; }
+      .mb-${gap} { margin-bottom: ${gap * 5}px; }
+      .ml-${gap} { margin-left: ${gap * 5}px; }
+      .ma-${gap} { margin: ${gap * 5}px; }
+      
+      .pt-${gap} { padding-top: ${gap * 5}px; }
+      .pr-${gap} { padding-right: ${gap * 5}px; }
+      .pb-${gap} { padding-bottom: ${gap * 5}px; }
+      .pl-${gap} { padding-left: ${gap * 5}px; }
+      .pa-${gap} { padding: ${gap * 5}px; }
+    `
+    : `
+      .mt-${gap} { margin-top: ${gap}; }
+      .mr-${gap} { margin-right: ${gap}; }
+      .mb-${gap} { margin-bottom: ${gap}; }
+      .ml-${gap} { margin-left: ${gap}; }
+      .ma-${gap} { margin: ${gap}; }
+      
+      .pt-${gap} { padding-top: ${gap}; }
+      .pr-${gap} { padding-right: ${gap}; }
+      .pb-${gap} { padding-bottom: ${gap}; }
+      .pl-${gap} { padding-left: ${gap}; }
+      .pa-${gap} { padding: ${gap}; }
+    `
+  )}
   
   .text--center {
     text-align: center;
